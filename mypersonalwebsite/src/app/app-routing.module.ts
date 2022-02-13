46afe9ee8267439857ca40156cd95fe8c2aafe9d
ycaktirma <fzmtechnology@FZMs-MacBook-Pro.local>
2022-02-14 00:14:05 +0300

    Routing configured for two pages: homepage and blog

--- mypersonalwebsite/src/app/app-routing.module.ts
+++ mypersonalwebsite/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'/homepage',
+    pathMatch:'full'
+  },
+  {
+    path:'homepage',
+    component:HomepageComponent
+  },
+  {
+    path:'blog',
+    component:BlogComponent
+  },
+  {
+    path:'**',
+    redirectTo:'/homepage',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mypersonalwebsite/src/app/app.module.ts
+++ mypersonalwebsite/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
